d93923fe7e465723863901612a869e680a92b67c
Rambo19971009 <995365320@qq.com>
2025-07-18 15:57:15 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,18 @@
             ></el-button>
           </div>
         </el-header>
-        <div class="main-content"></div>
+        <div class="main-content">
+          <el-row :gutter="10">
+            <el-col :span="6">
+              <div class="grid-content">
+                <WeightItem title="Lokalise" url="/src/assets/lokalise.svg" />
+              </div>
+            </el-col>
+            <el-col :span="6"><div class="grid-content" /></el-col>
+            <el-col :span="6"><div class="grid-content" /></el-col>
+            <el-col :span="6"><div class="grid-content" /></el-col>
+          </el-row>
+        </div>
       </el-main>
       <el-footer>
         <div class="footer_text">© Rambo Systems Software Ltd</div>
@@ -26,6 +37,7 @@
 
 <script setup>
 import { Setting } from "@element-plus/icons-vue";
+import WeightItem from "./Components/Weight-Item.vue";
 
 const handleSettingClick = () => {
   chrome.runtime.openOptionsPage();
@@ -44,6 +56,7 @@ const handleSettingClick = () => {
   padding: 0;
   height: 24px;
   display: flex;
+  margin-bottom: 10px;
   justify-content: space-between;
   .logo_icon {
     height: 24px;
@@ -76,5 +89,23 @@ const handleSettingClick = () => {
     font-size: 14px;
     line-height: 18px;
   }
+}
+.el-row {
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  background-color: #f3f5f6;
+  border-radius: 10px;
+  min-height: 72px;
+}
+.grid-content:hover {
+  background-color: #eaeced;
 }
 </style>
---
+++ src/Components/Weight-Item.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="weight-item">
+    <div class="weight-item-icon">
+      <el-image :src="url" :fit="fill" />
+    </div>
+    <div class="weight-item-title">
+      <span class="weight-item-title-text">
+        {{ title }}
+      </span>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<style lang="scss" scoped>
+.weight-item {
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 74px;
+  .weight-item-icon {
+    width: 24px;
+    height: 24px;
+    margin-bottom: 8px;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
+
+  .weight-item-title-text {
+    font-size: 12px;
+    color: #666666;
+  }
+}
+</style>
